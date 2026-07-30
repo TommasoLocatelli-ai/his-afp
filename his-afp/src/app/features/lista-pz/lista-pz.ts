66bfcff78078989e66f663e8d4802d27aff65ab3
TommasoLocatelli <tommaso.locatelli@mat.tn.it>
2026-07-30 16:24:13 +0000

    feat: ✨ aggiunto pulsante per la gestione del personale nella pagina principale

--- his-afp/src/app/features/lista-pz/lista-pz.ts
+++ his-afp/src/app/features/lista-pz/lista-pz.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TabellaPz } from '../../pattern/tabella-pz/tabella-pz';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'his-lista-pz',
-  imports: [TabellaPz],
+  imports: [TabellaPz, Button, RouterLink],
   templateUrl: './lista-pz.html',
   styleUrl: './lista-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
